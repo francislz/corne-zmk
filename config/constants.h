66038540b74272f0d2abb74f8909349d13f1dcb6
francislz <francisconeluiz@gmail.com>
2025-09-16 09:27:24 -0300

    feat: layout for 36 key corne

---
+++ config/constants.h
@@ -0,0 +1,37 @@
+#pragma once
+
+// Keys
+#define ___ &trans
+#define XXX &none
+
+// Layers
+// Must match order in which they are defined
+#define COL 0
+#define CAC 1
+#define QWE 2
+#define QAC 3
+#define ERG 4
+#define EAC 5
+#define SYM 6
+#define NAV 7
+#define NUM 8
+#define MOU 9
+#define GAM 10
+
+// Modifier keys
+#define GL LGUI
+#define CL LCTRL
+#define AL LALT
+#define SL LSHFT
+#define GR RGUI
+#define CR RCTRL
+#define AR RALT
+#define SR RSHFT
+
+// OS
+#define WINDOWS 0
+#define LINUX 1
+#define MAC_OS 2
+
+// Apps
+#define TMUX LC(LA(J)) // Tmux -> Ctrl + Alt + J
